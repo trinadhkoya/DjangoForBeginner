be56474d172e7b7244632403ac893a1470532c91
tkoya9 <trinadh@dotlabs.com>
2016-07-30 11:53:01 +0530

    first commit

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# these are all the views  for the polls project
+def index(request):
+    return HttpResponse("Hello you are at Polls index");
